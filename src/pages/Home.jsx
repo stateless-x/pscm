91088d4739adad3c6bd3c9a5b21cf573824653a4
xwanats <b.wanats@gmail.com>
2023-07-28 02:20:07 +0700

    change navbar layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import NavBar from "../components/NavBar";
-function Home(){
-    return (
-        <>
-            <NavBar/>
-            <h1>Home</h1>
-        </>
-    )
+import "../styles/home.css";
+import excellence from '../assets/excellence.svg'
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="message-container">
+          <div className="headings">
+            <h1>The Best Ceramic <br />Machinery In Thailand</h1>
+            <h3>
+              Reliable Machinery, Reliable Results. <br />
+              Ceramic Mastery. Versatile Applications. <br />
+              Engineered for Your Success.
+            </h3>
+          </div>
+          <div className="cta">
+            <img src={excellence} alt="Excellence since 1986" className="excellence" />
+          {/* <Button /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Home;

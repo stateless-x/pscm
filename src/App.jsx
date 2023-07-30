3a664ce8f4ad54044695e7546b13f4a0489e1c41
xwanats <b.wanats@gmail.com>
2023-07-30 20:35:35 +0700

    add why choose us section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
-import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from './pages/Home'
-import Contact from './pages/Contact';
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <>
@@ -11,15 +10,13 @@ function App() {
         <NavBar />
       </header>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact-us' element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact-us" element={<Contact />} />
         {/* <Route path='*' element={<NotFound/>} /> */}
       </Routes>
-      <footer>
-        {/* to be added */}
-      </footer>
+      <footer>{/* to be added */}</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

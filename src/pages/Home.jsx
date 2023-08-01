5cf9fe873d347a0a6c6788469e9e8779cfe7918e
xwanats <b.wanats@gmail.com>
2023-08-02 03:05:15 +0700

    add highlight items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ import Helmet from "react-helmet";
 import img1 from "../assets/service-1.jpg";
 import img2 from "../assets/service-2.jpg";
 import img3 from "../assets/service-3.jpg";
+import testImg1 from '../assets/test-img1.png';
+import testImg2 from '../assets/test-img2.jpg';
+
 
 function Home() {
   return (
@@ -23,6 +26,7 @@ function Home() {
       <Hero />
       <WhyUs />
       <OurServices />
+      <Highlight />
     </>
   );
 }
@@ -58,47 +62,72 @@ function Hero() {
 
 function WhyUs() {
   return (
-    <>
-      <div className="container why-us-container">
-        <h2>Why Choose Us For The Best Ceramic Solutions</h2>
-        <div className="why-us-description-container flex-column">
-          <h3 className="fw-light">
-            Leverage our decades-long expertise in the ceramic industry to gain
-            a competitive edge. Discover the difference we bring to your
-            operations.
-          </h3>
-          <Button aria-label="" message="Learn More" color="secondary" />
-        </div>
+    <div className="container why-us-container">
+      <h2>Why Choose Us For The Best Ceramic Solutions</h2>
+      <div className="why-us-description-container flex-column">
+        <h3 className="fw-light">
+          Leverage our decades-long expertise in the ceramic industry to gain a
+          competitive edge. Discover the difference we bring to your operations.
+        </h3>
+        <Button aria-label="" message="Learn More" color="secondary" />
       </div>
-    </>
+    </div>
   );
 }
 
 function OurServices() {
   return (
-    <>
-      <div className="container our-services-container">
-        <h2>What We Offer</h2>
-        <div className="card-list-container">
-          <ServiceCard
-            title="Custom Manufacturing"
-            desc="Crafting unique machinery solutions. Precision meets your production needs."
-            image={img1}
-          />
-          <ServiceCard
-            title="Reliable Repairs"
-            desc="Keeping your operations smooth. We ensure minimal downtime."
-            image={img2}
-          />
-          <ServiceCard
-            title="Essential Parts Supply"
-            desc="Quality parts on demand. Optimizing your production uptime."
-            image={img3}
-          />
-        </div>
+    <div className="container our-services-container">
+      <h2>What We Offer</h2>
+      <div className="card-list-container">
+        <ServiceCard
+          title="Custom Manufacturing"
+          desc="Crafting unique machinery solutions. Precision meets your production needs."
+          image={img1}
+        />
+        <ServiceCard
+          title="Reliable Repairs"
+          desc="Keeping your operations smooth. We ensure minimal downtime."
+          image={img2}
+        />
+        <ServiceCard
+          title="Essential Parts Supply"
+          desc="Quality parts on demand. Optimizing your production uptime."
+          image={img3}
+        />
       </div>
-    </>
+    </div>
   );
 }
 
+function Highlight() {
+  return (
+    <div className="container">
+      <div className="highlight-header">
+        <h2>Highlighting Key Solutions</h2>
+        <h3>A Glimpse into Our Extensive Offerings</h3>
+      </div>
+      <HighlightedItem />
+      <HighlightedItem />
+    </div>
+  );
+}
+
+function HighlightedItem() {
+  return (
+    <div className="highlighted-item-container">
+      <div className="highlighted-item-description">
+        <h3>Ball Mill</h3>
+        <h5>Grinding Materials</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+          autem provident corrupti itaque porro molestias quisquam odio velit
+          hic odit.
+        </p>
+        <Button aria-label="" message="Get More Details" color="primary" />
+      </div>
+      <img src={testImg1} alt="" />
+    </div>
+  );
+}
 export default Home;
